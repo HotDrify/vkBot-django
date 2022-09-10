005a4598ed282de00fbd39424643fd004ebf3918
HotDrify <105966726+HotDrify@users.noreply.github.com>
2022-09-10 10:22:45 +0300

    Update and rename sys.py to sysinfo.py

--- func/sysinfo.py
+++ func/sysinfo.py
@@ -10,13 +10,9 @@ memory_info = psutil.virtual_memory()
 disk_info = psutil.disk_usage("/")
 proc = subprocess.check_output(["ps -eo stime,time,user,args"], shell=True).decode("utf-8")
 uptime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(p.create_time()))
-
-log_str = "\n| CPU | RAM | DISK |\n"
+log_str = "Django Bot"
+log_str+= "\n| CPU | RAM | DISK |\n"
 log_str+= "| %dcore | %.2fG | %.2f |\n" % (psutil.cpu_count(logical=False), memory_info.total/1024/1024/1024, disk_info.total/1024/1024/1024)
 log_str+= "| %s%%  |   %s%%  |   %s%%  |\n" % (cup_per, memory_info.percent, disk_info.percent)
-log_str+= "<====PROC====>\n"
-log_str+= proc + "\n"
-log_str+= "<====UPTIME====>\n"
-log_str+= uptime
-def sys():
+def sysinfo():
 	return f"{log_str}"
