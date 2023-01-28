45b51fc63ddb9575ea92110ce808046e2850b073
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-01-28 12:14:53 +0300

    status fix
    updated api version (5.21 - 5.131)

--- func/status.py
+++ func/status.py
@@ -1,7 +1,5 @@
 import requests
-
 from func.regular import *
-
 def status(STATUS, TOKEN, BotID, regulars, count_message, start_bot):
 	status = regular(STATUS, BotID, count_message, start_bot, TOKEN, regulars)
 	st = requests.post('https://api.vk.com/method/status.set', params = {'access_token': TOKEN, 'v': 5.131, 'text': status})
